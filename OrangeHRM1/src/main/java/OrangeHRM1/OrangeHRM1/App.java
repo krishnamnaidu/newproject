44c7832d743ce2bc5995a274bc1c0e8a4865216e
JAWAHAR NAIDU.K <JAWAHAR NAIDU.K@DESKTOP-BDOLUHO>
2020-11-26 15:53:42 +0530

    OrangeHRM1

---
+++ OrangeHRM1/src/main/java/OrangeHRM1/OrangeHRM1/App.java
@@ -0,0 +1,13 @@
+package OrangeHRM1.OrangeHRM1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
